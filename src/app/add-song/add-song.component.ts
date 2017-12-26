bf81ba069cf3a588fbaaaea5b42d9731fec3b7a4
Jaroslaw Olewniczak <jarek104@gmail..com>
2017-12-26 15:06:02 -0500

    working

--- src/app/add-song/add-song.component.ts
+++ src/app/add-song/add-song.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Song } from '../data models/song';
 import { SongsService } from '../services/songs.service';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-song',
@@ -12,17 +14,39 @@ export class AddSongComponent implements OnInit {
   years = [];
   languages = ['English', 'Polish'];
   songToAdd= new Song('', '', '', '', '', '');
+  form: FormGroup;
+  
 
-  constructor(private songService: SongsService) {
+  constructor(private songService: SongsService, private formBuilder: FormBuilder, private router: Router) {
     for (let i = 2017; i > 1940; i--) {
       this.years.push(i);
     }
    }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      authorControl: [null, Validators.required],
+      titleControl: [null, Validators.required],
+      genreControl: [null, Validators.required],
+      yearControl: [null],
+      languageControl: [null, Validators.required],
+      linkControl: [null, Validators.required],
+    });
   }
   addSong() {
-    console.log(this.songToAdd);
-    this.songService.addSong(this.songToAdd);
+
+    this.songToAdd.Author = this.form.get("authorControl").value;
+    this.songToAdd.Title = this.form.get("titleControl").value;
+    this.songToAdd.Genre = this.form.get("genreControl").value;
+    this.songToAdd.Year = this.form.get("yearControl").value;
+    this.songToAdd.Language = this.form.get("languageControl").value;
+    this.songToAdd.Link = this.form.get("linkControl").value;
+    if(this.form.valid) {
+      this.songService.addSong(this.songToAdd);
+      this.router.navigate(['/data']);    
+    }
+  }
+  navigateBack() {
+    this.router.navigate(['/data']);   
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -36,7 +36,8 @@ const config = {
     CustomMatarialModule,
     AngularFireModule.initializeApp(config),
     AngularFirestoreModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [SongsService],
   bootstrap: [AppComponent]
